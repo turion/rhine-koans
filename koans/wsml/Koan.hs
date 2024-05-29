e43c142d0ea5829181596c075ee775f8fba3039c
Manuel Bärenz <m.baerenz@sonnen.de>
2024-05-29 16:34:06 +0200

    WIP

---
+++ koans/wsml/Koan.hs
@@ -0,0 +1,31 @@
+module Koan where
+
+import Wuss
+
+import Control.Concurrent (forkIO)
+import Control.Monad (forever, unless, void)
+import Data.Text (Text, pack)
+import Network.WebSockets (ClientApp, receiveData, sendClose, sendTextData)
+
+main :: IO ()
+-- main = runSecureClient "echo.websocket.org" 443 "/" ws
+-- main = runSecureClient "www.seismicportal.eu" 443 "/standing_order" ws
+main = runSecureClient "www.seismicportal.eu" 443 "/" ws -- try again at home
+-- main = runSecureClient "api2.poloniex.com" 443 "/" ws
+
+ws :: ClientApp ()
+ws connection = do
+    putStrLn "Connected!"
+
+    void . forkIO . forever $ do
+        message <- receiveData connection
+        print (message :: Text)
+
+    let loop = do
+            line <- getLine
+            unless (null line) $ do
+                sendTextData connection (pack line)
+                loop
+    loop
+
+    sendClose connection (pack "Bye!")
---
+++ koans/wsml/KoanWS.hs
@@ -0,0 +1,37 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+module Koan where
+
+
+--------------------------------------------------------------------------------
+import           Control.Concurrent  (forkIO)
+import           Control.Monad       (forever, unless)
+import   Control.Monad.IO.Class
+import           Data.Text           (Text)
+import qualified Data.Text           as T
+import qualified Data.Text.IO        as T
+import qualified Network.WebSockets  as WS
+
+
+--------------------------------------------------------------------------------
+app :: WS.ClientApp ()
+app conn = do
+    putStrLn "Connected!"
+
+    -- Fork a thread that writes WS data to stdout
+    _ <- forkIO $ forever $ do
+        msg <- WS.receiveData conn
+        liftIO $ T.putStrLn msg
+
+    -- Read from stdin and write to WS
+    let loop = do
+            line <- T.getLine
+            unless (T.null line) $ WS.sendTextData conn line >> loop
+
+    loop
+    WS.sendClose conn ("Bye!" :: Text)
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = WS.runClient "www.seismicportal.eu" 80 "/standing_order" app
