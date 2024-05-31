1d39dc8ce131ddc2b9a9d5245835eb55ad1815ed
Manuel Bärenz <m.baerenz@sonnen.de>
2024-05-31 09:32:39 +0200

    WIP WSML

--- koans/wsml/Koan.hs
+++ koans/wsml/Koan.hs
@@ -2,15 +2,15 @@ module Koan where
 
 import Wuss
 
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, threadDelay)
 import Control.Monad (forever, unless, void)
 import Data.Text (Text, pack)
 import Network.WebSockets (ClientApp, receiveData, sendClose, sendTextData)
 
 main :: IO ()
 -- main = runSecureClient "echo.websocket.org" 443 "/" ws
--- main = runSecureClient "www.seismicportal.eu" 443 "/standing_order" ws
-main = runSecureClient "www.seismicportal.eu" 443 "/" ws -- try again at home
+main = runSecureClient "www.seismicportal.eu" 443 "/standing_order/websocket" ws
+-- main = runSecureClient "www.seismicportal.eu" 443 "/" ws -- try again at home
 -- main = runSecureClient "api2.poloniex.com" 443 "/" ws
 
 ws :: ClientApp ()
@@ -21,11 +21,6 @@ ws connection = do
     message <- receiveData connection
     print (message :: Text)
 
-  let loop = do
-        line <- getLine
-        unless (null line) $ do
-          sendTextData connection (pack line)
-          loop
-  loop
+    threadDelay $ 1000 * 1000000
 
   sendClose connection (pack "Bye!")
