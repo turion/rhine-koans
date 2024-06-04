74a8b00f1ee93f89ae692710bcd39b422c2a3e5e
Manuel Bärenz <m.baerenz@sonnen.de>
2024-06-04 17:21:14 +0200

    Check for untracked diffs

--- check_diffs.sh
+++ check_diffs.sh
@@ -8,4 +8,8 @@ do
     diff --unchanged-line-format="" --old-line-format="< %L" --new-line-format="> %L" $koan $(dirname $koan)/solution/$(basename $koan) > diffs/$(dirname $koan)/diff.txt || true
 done
 
+# Check that we didn't forget to check in diffs
+test -z "$(git ls-files --others --exclude-standard -- diffs)"
+
+# Check that the diffs haven't changed
 git diff --exit-code
